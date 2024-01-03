49414cbe9ed36f7fedb84cd7a0613375995ddbcf
ferbarcante <barcantef@gmail.com>
2024-01-03 10:07:15 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
-		Seller sl = new Seller(23, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println(sl);
+		Seller seller = sellerDao.findById(1);
+		System.out.println(seller);
 	}
 
 }
